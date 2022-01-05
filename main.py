29e7de7026e170d8bc03c2ce92b70d036a50c920
swapnali2324 <86935682+swapnali2324@users.noreply.github.com>
2022-01-05 11:52:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
+from flask import Flask
 from datetime import datetime
 myobj = datetime.now()
 hour=myobj.hour
 minute= myobj.minute
 new=str(hour)+":"+str(minute)
-app = FastAPI()
 
 
-@app.get("/")
-async def root():
+app = Flask(__name__)
+
+@app.route('/')
+def index():
     return {"message": "Hello World","time":new}
+
+app.run(host='0.0.0.0', port=81)
